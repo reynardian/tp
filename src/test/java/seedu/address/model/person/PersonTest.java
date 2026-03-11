37c863dcfd0fb253aef9aea25ed616ce0b7c6931
Anna Tan <e1356946@u.nus.edu>
2026-03-11 08:13:49 +0800

    Fix person test

--- src/test/java/seedu/address/model/person/PersonTest.java
+++ src/test/java/seedu/address/model/person/PersonTest.java
@@ -92,7 +92,9 @@ public class PersonTest {
 
     @Test
     public void toStringMethod() {
-        String expected = Person.class.getCanonicalName() + "{name=" + ALICE.getName() + ", phone=" + ALICE.getPhone()
+        String expected = Person.class.getCanonicalName() + "{name=" + ALICE.getName()
+                + ", age=" + ALICE.getAge()
+                + ", phone=" + ALICE.getPhone()
                 + ", email=" + ALICE.getEmail() + ", address=" + ALICE.getAddress() + ", tags=" + ALICE.getTags()
                 + ", parentName=" + ALICE.getParentName() + ", parentPhone=" + ALICE.getParentPhone()
                 + ", parentEmail=" + ALICE.getParentEmail() + "}";
